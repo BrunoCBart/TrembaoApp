43fd5dfc52ca263b63390368834acfae98ce7e78
Bruno Bartolomeu <bruno.cbartlomeu@gmail.com>
2022-05-24 19:50:47 -0300

    test/fix(frontend) fix tests not waiting for satate update

--- app/frontend/src/__tests__/main.test.js
+++ app/frontend/src/__tests__/main.test.js
@@ -1,27 +1,24 @@
-import React from 'react'
-import { screen, cleanup } from '@testing-library/react'
-import userEvent from '@testing-library/user-event'
 // import App from '../App'
 import '@testing-library/jest-dom/extend-expect'
-import { renderWithProvider } from '../../utils/test/renderWithRouterAndProvider'
-import { foodOptions } from '../../utils/test/mocks/foodOptions'
+import { cleanup, screen, waitFor } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import mockAxios from 'axios'
-import { act } from 'react-dom/test-utils'
+import React from 'react'
+import { foodOptions } from '../../utils/test/mocks/foodOptions'
+import { renderWithProvider } from '../../utils/test/renderWithRouterAndProvider'
 import Main from '../pages/Main'
 const dailyOptions = ['Arroz', 'Feijão', 'Misturas', 'Guarnições', 'Bebidas']
 
-globalThis.IS_REACT_ACT_ENVIRONMENT = true
-
 global.setImmediate = global.setTimeout
 
-beforeEach(async () => {
+beforeEach(() => {
   mockAxios.get.mockImplementationOnce((url) => {
     if (url === '/foods') {
       return Promise.resolve({ data: foodOptions })
     }
   })
 
-  await act(async () => renderWithProvider(<Main />))
+  waitFor(() => renderWithProvider(<Main />))
 })
 
 afterEach(cleanup)
@@ -47,13 +44,13 @@ describe('Test main page (user order request page)', () => {
     await Promise.all(promises)
   })
 
-  it('Order button should open a form', () => {
+  it('Order button should open order form', () => {
     const orderButton = screen.getByText(/Pedir/)
     userEvent.click(orderButton)
     expect(screen.getByText(/Confirmar/)).toBeInTheDocument()
   })
 
-  it('Options should render on the food type', async () => {
+  it('Options should render in the food type', async () => {
     const rice = await screen.findByText(/^Arroz branco$/)
     expect(rice).toBeInTheDocument()
   })
