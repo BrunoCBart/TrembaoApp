ed2eac781348c5d481a92a47b38f56a8608210e2
Bruno Bartolomeu <bruno.cbartlomeu@gmail.com>
2022-06-01 10:25:58 -0300

    test(backend): fix integration tests

--- app/backend/build/services/Food.js
+++ app/backend/build/services/Food.js
@@ -14,23 +14,7 @@ const FoodSubType_1 = require("../database/models/FoodSubType");
 const FoodType_1 = require("../database/models/FoodType");
 class FoodService {
     constructor() {
-        this.getAll = () => __awaiter(this, void 0, void 0, function* () {
-            const foods = yield FoodType_1.default.findAll({
-                include: [
-                    {
-                        model: Food_1.default,
-                        as: 'foods',
-                        attributes: ['id', 'name', 'foodSubTypeId', 'checked', 'price']
-                    }
-                ],
-                order: [
-                    ['id', 'ASC']
-                ]
-            });
-            return foods;
-        });
         this.foodTypesReduce = (foods) => {
-            console.log(foods);
             return foods.reduce((acc, food) => {
                 if (!acc.find((f) => f.foodType === food.foodType)) {
                     acc.push({
@@ -38,11 +22,14 @@ class FoodService {
                         foods: []
                     });
                 }
-                console.log(acc.find(acc.find((f) => f.foodType === food.foodType)));
-                // acc.find((f: any) => f.foodType === food.foodType).foods.push(food)
+                acc.find((f) => f.foodType === food.foodType).foods.push(food);
                 return acc;
             }, []);
         };
+        this.getAll = () => __awaiter(this, void 0, void 0, function* () {
+            const foods = yield this.getAllFoods();
+            return this.foodTypesReduce(foods);
+        });
         this.getAllChecked = () => __awaiter(this, void 0, void 0, function* () {
             const foods = yield this.getAllFoods();
             const checkedFoods = foods.filter((food) => food.checked);
